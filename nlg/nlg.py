740593d07599010d333c9b4fe96db8859f2e7cc4
SysAdmin <SysAdmin@dev10>
2011-02-12 19:14:19 -0800

    basic nlg functionality added

--- nlg/nlg.py
+++ nlg/nlg.py
@@ -7,16 +7,19 @@ class NLG:
         pass
 
     def __listOutput(NLUinput,NLGinput):
+        print "There were many results:", NLG[0].value, "\n"
+        print "How many would you like to see?\n"
         pass
 
     def __printResults(NLUinput,NLGinput):
-        print NLG[0].value
+        print NLG[0].value, ":\n"
         if NLG[1].key == "results":
             for result in NLG[1].value:
-                print result + "\n"
+                print result, "\n"
         pass
 
     def __likeResponse(NLUinput,NLGinput):
+        print "That's nice.  Is there anything else you like?\n"
         pass
 
     def parseToOutput(self, NLUinput, NLGinput):
