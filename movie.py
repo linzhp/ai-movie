9e0f75bd26dad6570b90925ccae319a77ea13bc9
Zhongpeng Lin <zlin5@ucsc.edu>
2011-02-13 21:39:09 -0800

    more logging in movie.py

--- movie.py
+++ movie.py
@@ -9,17 +9,21 @@ logging.basicConfig(level=logging.DEBUG,
                     filemode='w')
 nlu = NLU()
 
-greeting=nlg.greet()
-logging.info(greeting)
 try:
+    greeting=nlg.greet()
+    logging.info('Bot: '+greeting)
     input = raw_input(greeting+'\n')
     while input is not None:
+        logging.info('User: '+input)
         nlu_out = nlu.process(input)
+        logging.debug('nlu_out: '+nlu_out)
         dm_out=dialogManager.input(nlu_out)
-        output=nlg.process(dm_out)
+        logging.debug('dm_out: '+dm_out)
+        output=nlg.process(nlu_out,dm_out)
         for dict in nlu_out:
             if dict.get("command")=="exit":
                 exit()
+        logging.info('Bot: '+output)
         input = raw_input(output+'\n')
 except EOFError:
     exit()
