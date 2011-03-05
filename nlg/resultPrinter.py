2f711f67125fcabebe6d0cca0a163fd61cb1686f
Riley <rileystroud@gmail.com>
2011-03-04 18:18:27 -0800

    Made pronouns work correctly. Adding a few more sentence files.  Still not complete/thurough
    all nouns in sentence files now must be {0[X][Y]}
    with X being the subject number
    with Y being 0 for subject and 1 for object in the sentence.

--- nlg/resultPrinter.py
+++ nlg/resultPrinter.py
@@ -1,6 +1,7 @@
 #import nltk
 import nlg_utils as nlgu
 
+pronounKeys = ["PREV_HE", "PREV_SHE", "PREV_IT"]
 pronounSubjects = {"PREV_HE":"he", "PREV_SHE":"she", "PREV_IT":"it"}
 pronounObjects = {"PREV_HE":"him","PREV_SHE":"her", "PREV_IT":"it"}
 
@@ -8,36 +9,43 @@ subjFromObj = {
                "actor":[],
                "part":[],
                "title":["director","actor","sort","genre","part"],
-               "director":[],
+               "director":["title"],
                "genre":["title","actor","director"],
-               "plot":[],
-               "voice actor":[]
+               "plot":["title"],
+               "voice actor":["part"]
                }
 
-def getPrintSentence(itemType, subjectType):
-    fileName = '../nlg/prs/'+subjectType+itemType + '_sentences.txt'
+def getPrintSentence(itemType, subjectList):
+    subjectString = "_".join(subjectList);
+    if len(subjectList)>0:
+        subjectString+='_'
+    fileName = '../nlg/prs/'+subjectString+itemType + '_sentences.txt'
     rstring = nlgu.get_random_line(fileName)
     if rstring == "":
         print "ERROR"
     return rstring
 
-def getSubject(NLUOutput, itemType):
+def getSubjectList(NLUOutput, itemType):
     # if the program fails here add another entry to the dict 
     checklist = subjFromObj[itemType] 
-    rstring = ""
+    rlist = []
     for key in NLUOutput[0].keys():
         if key in checklist:
-            rstring += key +'_'
-    return rstring
+            rlist.append(key)
+    return rlist
 
 def do(itemType, NLUOutput, resultList):
-    printSentence = getPrintSentence(itemType, getSubject(NLUOutput, itemType))
+    subject = getSubjectList(NLUOutput, itemType)
+
+    printSentence = getPrintSentence(itemType, subject)
 
     result = printItems(itemType, resultList)
 
-    pronouns = getNouns(NLUOutput, itemType)
+    pronouns = getNouns(NLUOutput, itemType, subject)
         
-    print printSentence.format(pronouns, result),
+    printSentence = printSentence.format(pronouns, result)
+    
+    print printSentence[:1].capitalize()+printSentence[1:],
 
 def printItems(itemType, resultList):
     if len(resultList)==1:
@@ -71,6 +79,15 @@ def printBigItemList(itemType, resultList):
 def printItem(itemType, result):
     return result
 
-def getNouns(NLUOutput, itemType):
+def getNouns(NLUOutput, itemType, subjectList):
+    returnList = []
+    for subject in subjectList:
+        a = []
+        if NLUOutput[0][subject] in pronounKeys:
+            a = [pronounSubjects[NLUOutput[0][subject]],pronounObjects[NLUOutput[0][subject]]]
+        else:
+            a = [NLUOutput[0][subject],NLUOutput[0][subject]]
+        returnList.append(a)
+    print subjectList, returnList
+    return returnList
     return ("he", "him", "it")
-    pass
