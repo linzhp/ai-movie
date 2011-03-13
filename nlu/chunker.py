1887875e8768deda42ea892c4763ca2617a1a106
Oresus <dmcgrego@unix.ic.ucsc.edu>
2011-03-13 16:23:03 -0700

    added a WORKING test mode

--- nlu/chunker.py
+++ nlu/chunker.py
@@ -1,17 +1,17 @@
 #
 # To instantiate a chunker:
 #
-#		x = chunker.Chunker()
+# x = chunker.Chunker()
 #
 # To Chunk:
 #
-#		x.chunk(string)
+# x.chunk(string)
 #
 # Example:
 #
-# 		import chunker
-# 		chk = chunker.Chunker()
-# 		chunk_tree = chk.chunk("Who directed \"The Big Lebowski\"?")
+# import chunker
+# chk = chunker.Chunker()
+# chunk_tree = chk.chunk("Who directed \"The Big Lebowski\"?")
 
 import nltk
 import pickle
@@ -22,60 +22,62 @@ class Chunker:
     def __init__(self, verbose=False, test = False):
 
         if verbose:
-        	print "#Initializing chunker:"
-
+            print "#Initializing chunker:"
+        
         # Define regular expressions for punctuation tags
         tagged_punc = [ (r'\"', ':'), (r'\?', 'QM') ]
+        
         if verbose:
-            print "   Loading POS tagged training sentences."
-
-# Load the tagged training sentences.
+            print " Loading POS tagged training sentences."
+        
+        # Load the tagged training sentences.
         if not test:
             f = open( path.join(path.dirname(__file__),'training_sentences.bin'), 'r' )
             train_sents = pickle.load(f)
         elif test:
-            train_sents = open('./shortsents','r')
-
-	
-		# Define the tagger.
-
+            f = open( path.join(path.dirname(__file__),'test_sentences'), 'r' )
+            train_sents = pickle.load(f)
+        
+        
+        # Define the tagger.
+        
         if verbose:
-            print "   Loading maxent_treebank_pos_tagger."
-
+            print " Loading maxent_treebank_pos_tagger."
+        
         _POS_TAGGER = 'taggers/maxent_treebank_pos_tagger/english.pickle'
         t1 = nltk.data.load(_POS_TAGGER)
         if verbose:
-        	print "   Training custom Unigram Tagger."		
+            print " Training custom Unigram Tagger."
         t2 = nltk.UnigramTagger( train_sents, backoff=t1 )
         
         if verbose:
-        	print "   Instantiating tagger object."
+            print " Instantiating tagger object."
         
         self.tagger = nltk.RegexpTagger( tagged_punc, backoff=t2 )
         
         # Define a chunking grammar.
         chunk_grammar = r"""
-        	B-QUESTION: {<WDT|WP|WRB><DT|RB.*|JJ>*<MD|VB.*|KW_.*>}
-        				{<WRB>}
-        	COMMAND: {^(<MD><PRP>)?(<RB>)*<VB|VBP>}
-        				{^<PRP><VB|VBP><TO>}
-        				{<RB><VBP|VB>}
-        	TITLE: {<:><[^:]*>*<:>}	
-        	PERSON: {<NNP[S]?>+}
-        	NP:   {<PRP\$>?<JJ>*<NN|NNS>(<POS>?<JJ>*<NN|NNS>)*}
-        	PP: { <IN><NP> }
+        B-QUESTION: {<WDT|WP|WRB><DT|RB.*|JJ>*<MD|VB.*|KW_.*>}
+        {<WRB>}
+        COMMAND: {^(<MD><PRP>)?(<RB>)*<VB|VBP>}
+        {^<PRP><VB|VBP><TO>}
+        {<RB><VBP|VB>}
+        TITLE: {<:><[^:]*>*<:>}
+        PERSON: {<NNP[S]?>+}
+        NP: {<PRP\$>?<JJ>*<NN|NNS>(<POS>?<JJ>*<NN|NNS>)*}
+        PP: { <IN><NP> }
         """
-        	#ACTOR_IN_MOVIE: {<PERSON><.*>*<IN><TITLE>}
-        	#S: {<CC><.*>*}
-           	#B-QUESTION: {^<[W].*|VBD|VBN|VBP|VBZ>}
+        #ACTOR_IN_MOVIE: {<PERSON><.*>*<IN><TITLE>}
+        #S: {<CC><.*>*}
+        #B-QUESTION: {^<[W].*|VBD|VBN|VBP|VBZ>}
         if verbose:
-        	print "   Instantiating RegexpParser object."
+            print " Instantiating RegexpParser object."
         
         # Define a regular expression chunker
         self.cp = nltk.RegexpParser(chunk_grammar)
         
         if verbose:
-        	print "Chunker Initialized.#"
+            print "Chunker Initialized.#"
     
     def chunk(self, sentence):
         tokd = nltk.word_tokenize(sentence)
@@ -89,12 +91,12 @@ class Chunker:
         return chunked
 
 if __name__ == '__main__':
-	with open(path.join(path.dirname(__file__), "chunkerpickler.bin"),'rb') as pickled_file:
-		chk = pickle.load(pickled_file)
-#	chk = Chunker(False)
-	result = chk.chunk("""Are there other movies that are similar to "Inception"?""")
-	print result
-	result.draw()
+    with open(path.join(path.dirname(__file__), "chunkerpickler.bin"),'rb') as pickled_file:
+        chk = pickle.load(pickled_file)
+        # chk = Chunker(False)
+        result = chk.chunk("""Are there other movies that are similar to "Inception"?""")
+        print result
+        result.draw()
 
 """
 I like movies like "God Father" or "The load of the rings".
