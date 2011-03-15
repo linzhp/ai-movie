3999e633621705add8541d2de6f6cd92fbaa135c
Oresus <dmcgrego@unix.ic.ucsc.edu>
2011-03-15 15:09:15 -0700

    changed greeting to not mislead with name

--- movie.py
+++ movie.py
@@ -13,7 +13,7 @@ dialogManager = DialogManager()
 #TODO test MORE PREF
 #TODO add catch all statement so that whenever it breaks, it will be off topic
 try:
-    greeting="Hi, there"#nlg.greet()
+    greeting="Hi, I am NIMROD, and you can ask me to help you find movies,\nTell me your preferences, and i can find movies that match them"#nlg.greet()
     logging.info('Bot: '+greeting)
 #    print('Bot: '+greeting)
     input = raw_input(greeting+'\n')
@@ -21,20 +21,20 @@ try:
         if len(input)==0:
             input = raw_input("Could you speak louder? I can't hear you\n")
             continue
-        try:
-            # NLU processing
-            logging.info('User: '+input)
-            nlu_out = nlu.process(input)
-            logging.debug('nlu_out: '+str(nlu_out))
-            # Dialog manager processing
-            dm_out=dialogManager.input(nlu_out)
-            logging.debug('dm_out: '+str(dm_out))
-            # Dialog manager gives feed back to NLU
-            nlu.expect = dialogManager.pending_question
-            # Generate response to user
-            output=nlg.process(nlu_out,dm_out)
-        except Exception, ex:
-            raise ex
+       # try:
+        # NLU processing
+        logging.info('User: '+input)
+        nlu_out = nlu.process(input)
+        logging.debug('nlu_out: '+str(nlu_out))
+        # Dialog manager processing
+        dm_out=dialogManager.input(nlu_out)
+        logging.debug('dm_out: '+str(dm_out))
+        # Dialog manager gives feed back to NLU
+        nlu.expect = dialogManager.pending_question
+        # Generate response to user
+        output=nlg.process(nlu_out,dm_out)
+#        except Exception, ex:
+#            raise ex
 #            output = chatbot.reply
         # Print and log response  
         logging.info('Bot: '+str(output))
