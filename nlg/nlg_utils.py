e7a549d61414e58c899463754fd4a08474a4f29a
andrewpsy <panshuyu@hotmail.com>
2011-03-16 19:03:37 -0700

    change import debugging to import logging in nlu/utils.py.

--- nlg/nlg_utils.py
+++ nlg/nlg_utils.py
@@ -1,6 +1,6 @@
 import string
 import random
-import debugging
+import logging
 
 ten = {1:"ten", 2:"twenty", 3:"thirty", 4:"fourty", 5:"fifty"}
 ones = {0:"zero", 1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine"}
